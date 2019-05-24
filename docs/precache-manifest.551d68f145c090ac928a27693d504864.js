6f22138ffe67cdc751e863d4b565770f654c8caa
carmen-tm <carmentorremolina@gmail.com>
2019-05-24 15:59:55 +0200

    Create initial production version

---
+++ docs/precache-manifest.551d68f145c090ac928a27693d504864.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1dd15645cb4e0d686268bad45b81e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005f999dc10ac4e63d5d",
+    "url": "/static/css/main.b2492f6f.chunk.css"
+  },
+  {
+    "revision": "e5af335abe34127f61b1",
+    "url": "/static/js/2.41de6b80.chunk.js"
+  },
+  {
+    "revision": "005f999dc10ac4e63d5d",
+    "url": "/static/js/main.74f16dc3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
