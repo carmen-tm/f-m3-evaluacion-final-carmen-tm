2783ac30e365d788f355b0f186287950047c2685
carmen-tm <carmentorremolina@gmail.com>
2019-05-27 12:33:22 +0200

    Add svg house images for each card

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import noun_Gryffindor from '../../images/noun_Gryffindor.svg';
+import noun_Hufflepuff from '../../images/noun_Hufflepuff.svg';
+import noun_Ravenclaw from '../../images/noun_Ravenclaw.svg';
+import noun_Slytherin from '../../images/noun_Slytherin.svg';
 
 import './styles.scss';
 
 const CharacterCard = props => {
 	const { character } = props;
 	const { id, name, image, house } = character;
+
+	const getHouseLogo = house => {
+		if (house === 'Gryffindor') {
+			return noun_Gryffindor;
+		} else if (house === 'Hufflepuff') {
+			return noun_Hufflepuff;
+		} else if (house === 'Ravenclaw') {
+			return noun_Ravenclaw;
+		} else if (house === 'Slytherin') {
+			return noun_Slytherin;
+		} else {
+			return 'https://via.placeholder.com/50/ffffff/808080 ?Text=No-house';
+		}
+	};
+
 	return (
 		<Link
 			to={`/character-detail/${id}`}
@@ -22,6 +41,11 @@ const CharacterCard = props => {
 					<p className="character-card__info">
 						{house ? house : <span>&nbsp;&nbsp;</span>}
 					</p>
+					<img
+						className="character-card__house"
+						src={getHouseLogo(house)}
+						alt={house}
+					/>
 				</main>
 			</div>
 		</Link>
